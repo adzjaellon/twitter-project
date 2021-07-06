e4fdf6eb7689983770e1649d3d22605a17198c14
Adrian <adisonator1@protonmail.com>
2021-07-06 15:30:30 +0200

    Adding new features to the admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'created']
+    list_filter = ['followers_only', 'created', 'author']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'created']
+    list_filter = ['created', 'author']
--- user_profile/models.py
+++ user_profile/models.py
@@ -22,4 +22,3 @@ class Profile(models.Model):
     class Meta:
         ordering = ('-created', )
 
-
